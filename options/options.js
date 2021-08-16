b1de5dec01916de7d55ac27ddaa3f05370983ac6
Crowdbotics <team@crowdbotics.com>
2021-08-16 01:50:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "solitary_hat_29841Identifier",
+  url: "https://solitary_hat_29841Identifier.botics.co",
+  api: "https://solitary_hat_29841Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
